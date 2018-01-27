a12c078e11db01ad76039ee420ffee86b54fdb29
themightykurt <30705411+themightykurt@users.noreply.github.com>
2018-01-27 15:45:53 -0500

    Adding GameManager, PoolManager, and OverlayManager. Example of overlay text in TestTrack.unity.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public BabyCarController babyCarController;
+
+    public int score;
+
+    public Camera ActiveCamera
+    {
+        get
+        {
+            return Camera.main;
+        }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        //The following is just testing to prove the concept of overlay elements - TODO: remove
+        if (babyCarController != null)
+        {
+            if (OverlayManager.instance != null)
+            {
+                OverlayManager.instance.SpawnOverlayText(babyCarController.transform, "CAR!", Color.blue, 50);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Managers/OverlayManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class OverlayManager : MonoBehaviour
+{
+    public static OverlayManager instance;
+    public List<OverlayElement> overlayElements = new List<OverlayElement>();
+
+    // Use this for initialization
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void LateUpdate()
+    {
+        overlayElements.RemoveAll(element => !element.Run());
+    }
+
+    public OverlayElement SpawnOverlayElement(string pooledName, Transform overlayParent, OverlayElement.OverlayMovement overlayType = OverlayElement.OverlayMovement.StayOnWorld)
+    {
+        OverlayElement offscreenOverlay = PoolManager.instance.SpawnPooledObject(pooledName).GetComponent<OverlayElement>();
+
+        offscreenOverlay.movementType = overlayType;
+        offscreenOverlay.worldParent = overlayParent;
+        offscreenOverlay.isActive = true;
+        offscreenOverlay.transform.parent = transform;
+        offscreenOverlay.transform.localScale = new Vector3(1, 1, 1);
+        overlayElements.Add(offscreenOverlay);
+        offscreenOverlay.Tick();
+
+        return offscreenOverlay;
+    }
+
+    public OverlayElement SpawnOverlayText(Transform overlayParent, string textContents, Color color, int fontSize, OverlayElement.OverlayMovement overlayType = OverlayElement.OverlayMovement.StayOnWorld)
+    {
+        OverlayElement offscreenOverlay = PoolManager.instance.SpawnPooledObject("overlayText").GetComponent<OverlayElement>();
+
+        Text text = offscreenOverlay.GetComponent<Text>();
+        text.text = textContents;
+        text.color = color;
+        text.fontSize = fontSize;
+
+        offscreenOverlay.movementType = overlayType;
+        offscreenOverlay.worldParent = overlayParent;
+        offscreenOverlay.isActive = true;
+        offscreenOverlay.transform.parent = transform;
+        offscreenOverlay.transform.localScale = new Vector3(1, 1, 1);
+        overlayElements.Add(offscreenOverlay);
+        offscreenOverlay.Tick();
+
+        return offscreenOverlay;
+    }
+}
