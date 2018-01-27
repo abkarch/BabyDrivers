00aba1fbb50e1c9f00a5f600ebb4ae910ce59f3e
GriffinGeiger <griffin2420@yahoo.com>
2018-01-27 12:44:28 -0500

    Damage effects prefab added

---
+++ Assets/Fx Explosion Pack/Script/Explosions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Explosions : MonoBehaviour
+{
+
+    public ParticleSystem explosions;
+    public ParticleSystem fire;
+
+    public AudioSource explosionSound;
+    public AudioSource babyScream;
+	
+    public void LowHealthFire()
+    {
+        fire.Play(true);
+    }
+    public void SetOffExplosions()
+    {
+        explosions.Play(true);
+        explosionSound.Play(0);
+        babyScream.Play(0);
+       
+    }
+	void Update () {
+        if (Input.GetKeyDown("space"))
+            SetOffExplosions();
+        if (Input.GetKeyDown("b"))
+            LowHealthFire();
+	}
+}
---
+++ Assets/Scripts/Car/ManualTransmission.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using System;
+
+public class ManualTransmission : MonoBehaviour
+{
+
+    public CarPedal clutchPedal;
+    
+    public void SwitchGears()
+    {
+        if(clutchPedal.PedalValue==1)
+        {
+            //Gear shifter is interactable
+        }
+    }
+
+
+
+}
